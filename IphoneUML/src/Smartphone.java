e1e22ff56341a046c38ad3c0f1933f61083d7c28
Kxllvin <kelvintenor987@gmail.com>
2024-05-23 11:52:12 -0300

    principal

--- IphoneUML/src/Smartphone.java
+++ IphoneUML/src/Smartphone.java
@@ -1,5 +1,21 @@
 public class Smartphone {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+            public static void main(String[] args) {
+                // Testando Reprodutor Musical
+                ReprodutorMusical reprodutor = new ReprodutorMusical();
+                reprodutor.selecionarMusica("Música 1");
+                reprodutor.tocar();
+                reprodutor.pausar();
+        
+                // Testando Aparelho Telefônico
+                Telefone telefone = new Telefone();
+                telefone.ligar("123456789");
+                telefone.atender();
+                telefone.iniciarCorreioVoz();
+        
+                // Testando Navegador na Internet
+                Navegador navegador = new Navegador();
+                navegador.exibirPagina("https://www.exemplo.com");
+                navegador.adicionarNovaAba();
+                navegador.atualizarPagina();
+            }
 }
